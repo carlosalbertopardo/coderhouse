2ccb0e2054f369912ff861bf6605c6a68f3b39a8
carlosalbertopardo <carlos.pardo@wunderman.com>
2017-06-01 15:15:02 -0300

    functional pokedex

--- 10-ProyectoFinal/pokedex/js/app.js
+++ 10-ProyectoFinal/pokedex/js/app.js
@@ -6,62 +6,14 @@ Trabajo Final: Pokedex!
 
 */
 
-
-function traerInformacion () {
-
-
-	//var page = 'offset=' + 20;
+function makeAjaxRequest(ajaxUrl, callback) {
 
 	$.ajax({
 		type: 'GET', //Tipo de operación que voy a realizar
 		dataType: "json", //El tipo de datos que voy a traer
-		url: 'http://pokeapi.co/api/v2/pokemon/', //La url de donde estan los datos
+		url: ajaxUrl, //La url de donde estan los datos
 		success: function (data) { //Lo que voy a hacer si llega bien la informacion
-			console.log(data.results);
-
-			var pokemonList = data.results;
-
-			for (var i = 0; i < pokemonList.length; i++) {
-				console.log(pokemonList[i].name);
-
-				//Cargar Titulo
-
-				var nameContainer = $('<a></a>');
-				nameContainer.html(pokemonList[i].name);
-				nameContainer.attr('href', pokemonList[i].url);
-
-
-				//Attach click event
-				nameContainer.click(function (event) {
-					
-					event.preventDefault();
-					var ajaxUrl = event.target.href;
-
-					showPokemon(ajaxUrl);
-
-				});
-
-				var pokemon = $('<li></li>');
-				pokemon.append(nameContainer);
-
-
-				$('.pokemon-list ul').append(pokemon);
-
-
-
-
-				//Cargar imagen?
-				/*
-				var imagenes = albums[i].images;
-				console.log(imagenes[0].url);
-				var imagen = $('<img>');
-				imagen.attr('src', imagenes[0].url );
-
-				$('body').append(imagen);
-*/
-			}
-
-
+			callback(data);
 		},
 		error: function (data) { //Lo que voy a hacer si llega mal la información
 			console.log(data);
@@ -69,9 +21,72 @@ function traerInformacion () {
 		}
 	});
 
+}
+
+
+function showPokemon(data) {
+
+	//Set Name
+	$('#poke-name').html(data.name);
+
+	//Set Type
+
+	var types = data.types;
+	$('.types').html('');
+	for (var i = 0; i < types.length; i++) {
+		var type = $('<li>'+ types[i].type.name +'</li>');
+		type.addClass(types[i].type.name);
+		$('.types').append(type);
+	}
+
+	//Set Image
+	var pokeImage = $('#poke-image');
+	pokeImage.attr('src',data.sprites.front_default);
+
+	//Set Number
+	$('#poke-number').html(data.id);
 
 }
 
+
+function showDescription (data) {
+	$('#poke-info').html(data.flavor_text_entries[1].flavor_text);
+}
+
+function addPokemons (data) {
+
+	console.log(data.results);
+
+	var pokemonList = data.results;
+
+	for (var i = 0; i < pokemonList.length; i++) {
+
+		//Cargar Titulo
+		var nameContainer = $('<a></a>');
+		nameContainer.html(pokemonList[i].name);
+		nameContainer.attr('href', pokemonList[i].url);
+
+		//Attach click event
+		nameContainer.click(function (event) {
+			
+			event.preventDefault();
+			var ajaxUrl = event.target.href;
+
+			makeAjaxRequest(ajaxUrl, showPokemon );
+
+			var descriptionUrl = ajaxUrl.replace("pokemon", "pokemon-species");
+			makeAjaxRequest(descriptionUrl, showDescription)
+
+		});
+
+		var pokemon = $('<li></li>');
+		pokemon.append(nameContainer);
+
+		$('.pokemon-list ul').append(pokemon);
+
+	}
+
+}
 
 
 
@@ -79,7 +94,8 @@ function traerInformacion () {
 
 	$(document).ready(function () {
 
-		traerInformacion();
+		//traerInformacion();
+		makeAjaxRequest('http://pokeapi.co/api/v2/pokemon/', addPokemons);
 
 	})
 
